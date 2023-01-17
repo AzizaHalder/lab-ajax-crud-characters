9eab65d54c92c626420587ec0f1559186af21fd2
AzizaHalder <azizahalder9@gmail.com>
2023-01-17 22:55:15 +0100

    fetch one done, fetch all started

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,43 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      axios.get(`http://localhost:8000/characters`).then((response) => {});
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      const characterID = document.getElementById("search-by-id").value;
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+      axios
+        .get(`http://localhost:8000/characters/${characterID}`)
+        .then((response) => {
+          const characterDetail = response.data;
+          console.log(characterDetail);
+          document.getElementById("name").innerText = characterDetail.name;
+          document.getElementById("occupation").innerText =
+            characterDetail.occupation;
+          document.getElementById("cartoon").innerText =
+            characterDetail.cartoon;
+          document.getElementById("weapon").innerText = characterDetail.weapon;
+        })
+        .catch((error) => {
+          console.log("An error occurred when fetching the character", error);
+        });
+    });
 
-  });
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {});
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {});
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {});
 });
